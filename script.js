fb349e31121744755f520a9559e07de8053c3ef9
logicsmash <142871243+logicsmash@users.noreply.github.com>
2023-08-22 15:12:27 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculate() {
+    //定義
+    const AVERAGE_BULLET = 1100;
+    const ADD_TIME = 15;
+
+    // 入力された数量を取得
+    let peopleCount = parseInt(document.getElementById('peopleCount').value, 10);
+    let clearTime = parseFloat(document.getElementById('clearTime').value);
+    let difficulty = parseFloat(document.getElementById('difficulty').value);
+
+    // 合計獲得弾数を計算
+    let totalGetBullet = Math.round(AVERAGE_BULLET * peopleCount);
+
+    // 合計消費弾数を計算
+    let realTime = clearTime + ADD_TIME;
+    let totalUseBullet = Math.round(difficulty * (7200 / realTime));
+
+    //必要サブ数
+    let needSub = Math.ceil((totalUseBullet - totalGetBullet) / AVERAGE_BULLET);
+
+    // 結果を出力
+    document.getElementById('totalGetBullet').innerText = totalGetBullet;
+    document.getElementById('totalUseBullet').innerText = totalUseBullet;
+    document.getElementById('needSub').innerText = needSub;
+}
