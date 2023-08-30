ab839728ee550f92a77cd3984aa721690d74f757
logicsmash <142871243+logicsmash@users.noreply.github.com>
2023-08-30 12:12:45 +0900

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ function calculate() {
 
     // 入力された数量を取得
     let peopleCount = parseInt(document.getElementById('peopleCount').value, 10);
+    let clearTime = parseFloat(document.getElementById('haveBullet').value);
     let clearTime = parseFloat(document.getElementById('clearTime').value);
     let difficulty = parseFloat(document.getElementById('difficulty').value);
 
     // 合計獲得弾数を計算
-    let totalGetBullet = Math.round(AVERAGE_BULLET * peopleCount);
+    let totalGetBullet = Math.round(AVERAGE_BULLET * peopleCount) + haveBullet;
 
     // 合計消費弾数を計算
     let realTime = clearTime + ADD_TIME;
